64fc0764439460aed9c3b8361012f869951ba119
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-03 08:12:03 -0500

    adds layout and card wrappers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/allMeetups'
 import NewMeetupPage from './pages/newMeetup'
 import FavoritesPage from './pages/favorites'
-import MainNavigation from './components/layout/mainNavigation'
+import Layout from './components/layout/layout'
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
@@ -19,7 +18,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   )
 }
 
