b6a5436ae22283a98b8277b634ada2d8db9e622e
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-02 22:16:10 -0500

    adds react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Todo from './components/todo'
+import { Route, Switch } from 'react-router-dom'
+import AllMeetupsPage from './pages/allMeetups'
+import NewMeetupPage from './pages/newMeetup'
+import FavoritesPage from './pages/favorites'
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo title="Learn React" />
-      <Todo title="Master React" />
-      <Todo title="Explore full React course" />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   )
 }
---
+++ src/pages/newMeetup.js
@@ -0,0 +1,3 @@
+export default function NewMeetupPage() {
+  return <h1>New Meetup Page</h1>
+}
