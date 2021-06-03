d2716c8d9883e76a016952b6b8fc85b94140bd08
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-02 21:42:00 -0500

    adds state to modal

--- src/components/todo.js
+++ src/components/todo.js
@@ -9,6 +9,9 @@ export default function Todo(props) {
     setModalIsOpen(true)
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false)
+  }
   return (
     <div className="card">
       <h2>{props.title}</h2>
@@ -17,8 +20,10 @@ export default function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   )
 }
