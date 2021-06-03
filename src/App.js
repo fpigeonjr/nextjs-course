61bdb042bf0f3c2283a38442f8949cd12766de5c
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-02 22:24:10 -0500

    adds routes and navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/allMeetups'
 import NewMeetupPage from './pages/newMeetup'
 import FavoritesPage from './pages/favorites'
+import MainNavigation from './components/layout/mainNavigation'
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
