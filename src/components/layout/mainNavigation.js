5aaca3bc5df6b0a35e7b559f3656e2fe3bda0a32
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-06-05 19:19:01 -0500

    adds bade to fav naviagtion

--- src/components/layout/mainNavigation.js
+++ src/components/layout/mainNavigation.js
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
 import classes from './mainNavigation.module.css'
+import { useContext } from 'react'
+import FavoritesContext from '../../store/favorites-context'
 
-export default function MainNavigation() {
+function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext)
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -14,10 +18,17 @@ export default function MainNavigation() {
             <Link to="/new-meetup">New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <Link to="/favorites">
+              Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
     </header>
   )
 }
+
+export default MainNavigation
